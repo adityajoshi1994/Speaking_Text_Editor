284181030ee824a37a678a79d9d73317a04756e4
adityajoshi1994 <adityarajendrajoshi@gmail.com>
2018-10-31 19:57:04 -0700

    Initial Commit

---
+++ SpeakingTextEditor/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SpeakingTextEditor
+//
+//  Created by Aditya Joshi on 10/31/18.
+//  Copyright © 2018 Aditya Joshi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SpeakingTextEditor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SpeakingTextEditor
+//
+//  Created by Aditya Joshi on 10/31/18.
+//  Copyright © 2018 Aditya Joshi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
